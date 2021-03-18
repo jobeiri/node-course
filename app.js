a89ab6b19a9d5cfcdc1b6ff79105e04e3e3d8aa3
Amir Hossein Jobeiri <jobeiri.programmer@gmail.com>
2021-03-19 01:32:52 +0330

    4. Importing npm Modules

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+const validator = require("validator");
 const getNotes = require("./notes.js");
+
 const message = getNotes();
 console.log(message);
+
+console.log(validator.isURL("example.com"));
