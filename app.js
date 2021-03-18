dfccf288bb24bce994aab94c1061cba9bb0da956
Amir Hossein Jobeiri <jobeiri.programmer@gmail.com>
2021-03-19 01:08:18 +0330

    3. Importing Your Own Files

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const fs = require("fs");
-
-fs.writeFileSync("notes.txt", "My name is Amir Hossein Jobeiri.");
-fs.appendFileSync("notes.txt", " My name is Amir Hossein Jobeiri.");
+const getNotes = require("./notes.js");
+const message = getNotes();
+console.log(message);
