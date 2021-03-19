99b4a9f9cf76db3aa0f6152848644aeda2d1d856
Amir Hossein Jobeiri <jobeiri.programmer@gmail.com>
2021-03-19 10:40:32 +0330

    6. Global npm Modules and nodemon

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const getNotes = require("./notes.js");
 const message = getNotes();
 console.log(message);
 
-console.log(chalk.green.inverse.bold("Success"));
+console.log(chalk.red.inverse.bold("Error"));
