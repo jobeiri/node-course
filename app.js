fbac9d3a810d24a5b9bcec5737fc2d34ec109e56
Amir Hossein Jobeiri <jobeiri.programmer@gmail.com>
2021-03-19 10:31:18 +0330

    5. Printing in Color

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const validator = require("validator");
+const chalk = require("chalk");
 const getNotes = require("./notes.js");
 
 const message = getNotes();
 console.log(message);
 
-console.log(validator.isURL("example.com"));
+console.log(chalk.green.inverse.bold("Success"));
